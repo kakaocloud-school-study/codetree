849b316ebb480e6894cbeaf81b40b6824d429877
Sang Min Park <minisstar@naver.com>
2024-06-21 20:09:55 +0900

    격자안에서 완전탐색 풀이

--- 격자안에서완전탐색/행복한 수열의 개수.py
+++ 격자안에서완전탐색/행복한 수열의 개수.py
@@ -4,19 +4,20 @@ grid = [list(map(int, input().split())) for _ in range(n)]
 
 # 행복한 수열인지 판별하는 함수
 def is_happy_sequence(sequence):
-    max_length = 1
-    current_length = 1
+    max_length = 1  # 최대 연속 길이를 기록하는 변수, 초기값은 1로 설정
+    current_length = 1  # 현재 연속 길이를 기록하는 변수, 초기값은 1로 설정
     
+    # 시퀀스의 두 번째 요소부터 시작해서 순회
     for i in range(1, len(sequence)):
-        if sequence[i] == sequence[i - 1]:
-            current_length += 1
+        if sequence[i] == sequence[i - 1]:  # 현재 요소가 이전 요소와 같다면
+            current_length += 1  # 현재 연속 길이를 증가시킴
         else:
-            current_length = 1
+            current_length = 1  # 현재 요소가 이전 요소와 다르면 연속 길이를 1로 초기화
         
-        if current_length > max_length:
-            max_length = current_length
+        if current_length > max_length:  # 현재 연속 길이가 최대 연속 길이보다 크다면
+            max_length = current_length  # 최대 연속 길이를 현재 연속 길이로 업데이트
     
-    return max_length >= m
+    return max_length >= m  # 최대 연속 길이가 m 이상이면 True를 반환
 
 num_happy = 0
 
