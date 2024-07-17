a43aa054204c0097b774d75849b0f44eedb774d9
JHyun0302 <wogustkfkd11@gmail.com>
2024-07-17 15:14:37 +0900

    Backtracking : 순열 만들기

---
+++ Backtracking/순열 만들기/크기가_n인_순열.java
@@ -0,0 +1,40 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static final int MAX_N = 8;
+
+    static int n;
+    static boolean[] visited = new boolean[MAX_N + 1];
+    static List<Integer> picked = new ArrayList<>();
+
+    static void getPermutation(int cnt) {
+        if (cnt == n) {
+            for (int i = 0; i < picked.size(); i++) {
+                System.out.print(picked.get(i) + " ");
+            }
+            System.out.println();
+            return;
+        }
+
+        for (int i = 1; i <= n; i++) {
+            if (visited[i]) {
+                continue;
+            }
+            visited[i] = true;
+            picked.add(i);
+
+            getPermutation(cnt + 1);
+
+            visited[i] = false;
+            picked.remove(picked.size() - 1);
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+
+        getPermutation(0);
+    }
+}
